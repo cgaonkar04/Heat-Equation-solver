f952835be4a724d8a21c745382b58e3bdd92d74d
cgaonkar04 <113157887+cgaonkar04@users.noreply.github.com>
2024-05-19 20:47:27 +0530

    Update and rename 2d solver.py to 2d solver(Plate).py

---
+++ 2d solver(Plate).py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# System params
+
+a = 110
+length = 50 #mm
+time = 4 #se
+nodes = 40
+
+# Initialization 
+
+dx = length / nodes
+dy = length / nodes
+
+dt = min(   dx**2 / (4 * a),     dy**2 / (4 * a))
+
+t_nodes = int(time/dt)
+#initial conditions
+
+
+
+u = np.zeros((nodes, nodes)) + 20
+
+# Boundary Conditions 
+
+u[0, :] = np.linspace(0, 100, nodes)
+u[-1, :] = np.linspace(0, 100, nodes)
+
+u[:, 0] = np.linspace(0, 100, nodes)
+u[:, -1] = np.linspace(0, 100, nodes)
+
+# Visualisation setup
+
+fig, axis = plt.subplots()
+
+pcm = axis.pcolormesh(u, cmap=plt.cm.jet, vmin=0, vmax=100)
+plt.colorbar(pcm, ax=axis)
+
+# Simulation
+
+counter = 0
+
+while counter < time :
+
+    w = u.copy()
+
+    for i in range(1, nodes - 1):
+        for j in range(1, nodes - 1):
+
+            dd_ux = (w[i-1, j] - 2*w[i, j] + w[i+1, j])/dx**2
+            dd_uy = (w[i, j-1] - 2*w[i, j] + w[i, j+1])/dy**2
+
+            u[i, j] = dt * a * (dd_ux + dd_uy) + w[i, j]
+
+    counter += dt
+    #displaying average temp of the plate in the terminal
+    print("t: {:.3f} [s], Average temperature: {:.2f} Celsius".format(counter, np.average(u)))
+
+    # Updating the plot
+
+    pcm.set_array(u)
+    axis.set_title("Distribution at t: {:.3f} [s].".format(counter))
+    plt.pause(0.01)
+
+
+plt.show()
+
+
+
